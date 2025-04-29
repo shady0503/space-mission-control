2d81e8ef6717c2ee8d48e5cbc50ebb43a2037e3a
saadalaoui23 <saadalaouisosse@gmail.com>
2025-04-29 20:38:45 +0100

    initial commit

---
+++ entreprise/src/main/java/com/entreprise/EntrepriseApplication.java
@@ -0,0 +1,14 @@
+package com.entreprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class EntrepriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrepriseApplication.class, args);
+	}
+
+}
